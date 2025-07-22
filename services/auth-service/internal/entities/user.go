022ae4070b3bc9bd5a37c9834cf73f489cb4520b
Rhythin <rhythin.renny@converj.com>
2025-07-22 09:21:04 +0530

    defined user

--- services/auth-service/internal/entities/user.go
+++ services/auth-service/internal/entities/user.go
@@ -1,7 +1,19 @@
 package entities
 
+import (
+	"github.com/rhythin/bookspot/services/shared/custommodel"
+)
+
 type User struct {
-	Email    string `gorm:"not null"`
-	Name     string `gorm:"not null"`
-	Password string `gorm:"not null"`
+	custommodel.CustomModel
+	Username      string `gorm:"uniqueIndex;not null" json:"username"`
+	Email         string `gorm:"not null" json:"email"`
+	FirstName     string `json:"first_name"`
+	LastName      string `json:"last_name"`
+	PasswordHash  string `gorm:"not null" json:"-"`
+	PasswordSalt  string `gorm:"not null" json:"-"`
+	LoginAttempts int    `gorm:"default:0" json:"login_attempts"`
+	IsLocked      bool   `gorm:"default:false" json:"is_locked"`
+	IsAdmin       bool   `gorm:"default:false" json:"is_admin"`
+	TempToken     string `gorm:"type:text" json:"-"`
 }
